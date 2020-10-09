59883d968f1b72bf45b2d43e5ef0750aa7b4aac7
ShivamKumarJha <jha.shivam3@gmail.com>
2020-10-10 00:02:38 +0530

    Add repository
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ app/src/main/java/com/shivamkumarjha/oneplusfirmware/repository/PhoneInfoRepository.kt
@@ -0,0 +1,40 @@
+package com.shivamkumarjha.oneplusfirmware.repository
+
+import com.shivamkumarjha.oneplusfirmware.model.PhoneInfo
+import com.shivamkumarjha.oneplusfirmware.network.ApiListener
+import com.shivamkumarjha.oneplusfirmware.network.ApiService
+import com.shivamkumarjha.oneplusfirmware.network.NoConnectivityException
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+object PhoneInfoRepository {
+
+    var phoneInfo: PhoneInfo? = null
+
+    suspend fun getPhoneInfo(apiListener: ApiListener) =
+        withContext(Dispatchers.Default) {
+            ApiService.create().getPhoneInfo().enqueue(object : Callback<PhoneInfo> {
+                override fun onResponse(call: Call<PhoneInfo>, response: Response<PhoneInfo>?) {
+                    if (response != null) {
+                        if (response.code() == 200) {
+                            phoneInfo = response.body()
+                            apiListener.onResponse(response.body())
+                        } else
+                            apiListener.onResponseError(response.code())
+                    }
+                }
+
+                override fun onFailure(call: Call<PhoneInfo>, t: Throwable?) {
+                    if (t is NoConnectivityException) {
+                        apiListener.onOffline(t.message)
+                    } else {
+                        apiListener.onFailure(t?.localizedMessage.toString())
+                    }
+                }
+            })
+        }
+    
+}
---
+++ app/src/main/java/com/shivamkumarjha/oneplusfirmware/repository/PhoneModelsRepository.kt
@@ -0,0 +1,39 @@
+package com.shivamkumarjha.oneplusfirmware.repository
+
+import com.shivamkumarjha.oneplusfirmware.model.OneplusPhones
+import com.shivamkumarjha.oneplusfirmware.network.ApiListener
+import com.shivamkumarjha.oneplusfirmware.network.ApiService
+import com.shivamkumarjha.oneplusfirmware.network.NoConnectivityException
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+object PhoneModelsRepository {
+
+    var oneplusPhones: OneplusPhones? = null
+
+    suspend fun getOneplusPhones(apiListener: ApiListener) =
+        withContext(Dispatchers.Default) {
+            ApiService.create().getPhoneModels().enqueue(object : Callback<OneplusPhones> {
+                override fun onResponse(call: Call<OneplusPhones>, response: Response<OneplusPhones>?) {
+                    if (response != null) {
+                        if (response.code() == 200) {
+                            oneplusPhones = response.body()
+                            apiListener.onResponse(response.body())
+                        } else
+                            apiListener.onResponseError(response.code())
+                    }
+                }
+
+                override fun onFailure(call: Call<OneplusPhones>, t: Throwable?) {
+                    if (t is NoConnectivityException) {
+                        apiListener.onOffline(t.message)
+                    } else {
+                        apiListener.onFailure(t?.localizedMessage.toString())
+                    }
+                }
+            })
+        }
+}
